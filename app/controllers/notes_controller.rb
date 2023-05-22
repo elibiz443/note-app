fc6236ca254f396faee0c6c0564d64341ff9697b
elibiz443 <ellyambet10@gmail.com>
2023-05-22 07:46:36 +0300

    Added CRUD to note controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,7 +1,50 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
   def index
+    @notes = Note.all
   end
 
   def show
+  end
+
+  def new
+    @note = Note.new
+  end
+
+  def create
+    @note = Note.new(note_params)
+
+    if @note.save
+      redirect_to notes_path, notice: "Note was successfully created."
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @note.update(note_params)
+      redirect_to notes_path, notice: "Note was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @note.destroy
+    redirect_to notes_path, notice: "Note was successfully destroyed."
+  end
+
+  private
+
+  def set_note
+    @note = Note.find(params[:id])
+  end
+
+  def note_params
+    params.require(:note).permit(:name)
   end
 end
